1c54072604823fde73f2be17689f6ee51ee1609b
paranormallife <elliottoman@gmail.com>
2019-07-28 06:28:20 -0400

    404 Page

---
+++ wp-content/themes/asw_2019/404.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+<!-- Index Template -->
+
+<main class="index">
+    <div class="post-content">
+        <article>
+            <h1>404</h1>
+            <h2>Page not found</h2>
+            <p>
+                The content you are looking for does not exist or has been removed.
+            </p>
+            <p>
+                <a href="<?php echo get_bloginfo('wpurl'); ?>">&larr; Return to the homepage?</a>
+            </p>
+        </article>
+    </div>
+</main>
+
+<?php get_footer(); ?>
